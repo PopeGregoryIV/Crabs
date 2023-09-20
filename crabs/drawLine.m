38dddc7da772bbd9141d72c38e537f69a5589b5c
PopeGregoryIV <145616606+PopeGregoryIV@users.noreply.github.com>
2023-09-20 14:35:54 -0700

    added all code to crabs

---
+++ crabs/drawLine.m
@@ -0,0 +1,8 @@
+function lineHandle = drawLine ( pointA, pointB, color)
+
+x= [ pointA(1); pointB(1)];
+y= [ pointA(2); pointB(2)];
+
+lineHandle = plot (x,y, color);
+set(lineHandle,"LineWidth",3);
+endfunction
